2c19523ac238522523c780bfc0a81215436883a7
Dominique Feyer <dfeyer@ttree.ch>
2015-07-02 00:16:57 +0200

    [TASK] Move proxy related functions to the proxy module

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"github.com/dfeyer/flow-debugproxy/errorhandler"
 	"github.com/dfeyer/flow-debugproxy/logger"
-	"github.com/dfeyer/flow-debugproxy/pathmapper"
+	"github.com/dfeyer/flow-debugproxy/xdebugproxy"
 
-	"fmt"
 	"github.com/codegangsta/cli"
-	"io"
 	"net"
 	"os"
 )
@@ -78,120 +76,16 @@ func main() {
 			}
 			connid++
 
-			proxy := &proxy{
-				lconn:  conn,
-				laddr:  laddr,
-				raddr:  raddr,
-				erred:  false,
-				errsig: make(chan bool),
+			proxy := &xdebugproxy.Proxy{
+				Lconn:  conn,
+				Laddr:  laddr,
+				Raddr:  raddr,
+				Erred:  false,
+				Errsig: make(chan bool),
 			}
-			go proxy.start()
+			go proxy.Start()
 		}
 	}
 
 	app.Run(os.Args)
-}
-
-//A proxy represents a pair of connections and their state
-type proxy struct {
-	sentBytes     uint64
-	receivedBytes uint64
-	laddr, raddr  *net.TCPAddr
-	lconn, rconn  *net.TCPConn
-	erred         bool
-	errsig        chan bool
-}
-
-func (p *proxy) log(s string, args ...interface{}) {
-	if verbose {
-		logger.Info(s, args...)
-	}
-}
-
-func (p *proxy) err(s string, err error) {
-	if p.erred {
-		return
-	}
-	if err != io.EOF {
-		logger.Warn(s, err)
-	}
-	p.errsig <- true
-	p.erred = true
-}
-
-func (p *proxy) start() {
-	defer p.lconn.Close()
-	//connect to remote
-	rconn, err := net.DialTCP("tcp", nil, p.raddr)
-	if err != nil {
-		p.err("Remote connection failed: %s", err)
-		return
-	}
-	p.rconn = rconn
-	defer p.rconn.Close()
-	//display both ends
-	p.log("Opened %s >>> %s", p.lconn.RemoteAddr().String(), p.rconn.RemoteAddr().String())
-	//bidirectional copy
-	go p.pipe(p.lconn, p.rconn)
-	go p.pipe(p.rconn, p.lconn)
-	//wait for close...
-	<-p.errsig
-	p.log("Closed (%d bytes sent, %d bytes recieved)", p.sentBytes, p.receivedBytes)
-}
-
-func (p *proxy) pipe(src, dst *net.TCPConn) {
-	//data direction
-	var f, h string
-	isFromDebugger := src == p.lconn
-	if isFromDebugger {
-		f = "\nDebugger >>> IDE\n================"
-	} else {
-		f = "\nIDE >>> Debugger\n================"
-	}
-	h = "%s"
-	//directional copy (64k buffer)
-	buff := make([]byte, 0xffff)
-	for {
-		n, err := src.Read(buff)
-		if err != nil {
-			p.err("Read failed '%s'\n", err)
-			return
-		}
-		b := buff[:n]
-		p.log(h, f)
-		if veryverbose {
-			if isFromDebugger {
-				p.log("Raw protocol:\n%s\n", logger.Colorize(fmt.Sprintf(h, b), "blue"))
-			} else {
-				p.log("Raw protocol:\n%s\n", logger.Colorize(fmt.Sprintf(h, logger.FormatTextProtocol(b)), "blue"))
-			}
-		}
-		//extract command name
-		if isFromDebugger {
-			b = pathmapper.ApplyMappingToXML(b)
-		} else {
-			b = pathmapper.ApplyMappingToTextProtocol(b)
-		}
-		//show output
-		if veryverbose {
-			if isFromDebugger {
-				p.log("Processed protocol:\n%s\n", logger.Colorize(fmt.Sprintf(h, b), "blue"))
-			} else {
-				p.log("Processed protocol:\n%s\n", logger.Colorize(fmt.Sprintf(h, logger.FormatTextProtocol(b)), "blue"))
-			}
-		} else {
-			p.log(h, "")
-		}
-		//write out result
-		n, err = dst.Write(b)
-		if err != nil {
-			p.err("Write failed '%s'\n", err)
-			return
-		}
-		if isFromDebugger {
-			p.sentBytes += uint64(n)
-		} else {
-			p.receivedBytes += uint64(n)
-		}
-	}
 }
---
+++ xdebugproxy/xdebugproxy.go
@@ -0,0 +1,118 @@
+package xdebugproxy
+
+import (
+	"github.com/dfeyer/flow-debugproxy/logger"
+	"github.com/dfeyer/flow-debugproxy/pathmapper"
+
+	"fmt"
+	"io"
+	"net"
+)
+
+//todo replace by config module when ready
+var verbose = true
+var veryverbose = true
+
+//Proxy represents a pair of connections and their state
+type Proxy struct {
+	sentBytes     uint64
+	receivedBytes uint64
+	Laddr, Raddr  *net.TCPAddr
+	Lconn, rconn  *net.TCPConn
+	Erred         bool
+	Errsig        chan bool
+}
+
+func (p *Proxy) log(s string, args ...interface{}) {
+	if verbose {
+		logger.Info(s, args...)
+	}
+}
+
+func (p *Proxy) err(s string, err error) {
+	if p.Erred {
+		return
+	}
+	if err != io.EOF {
+		logger.Warn(s, err)
+	}
+	p.Errsig <- true
+	p.Erred = true
+}
+
+func (p *Proxy) Start() {
+	defer p.Lconn.Close()
+	//connect to remote
+	rconn, err := net.DialTCP("tcp", nil, p.Raddr)
+	if err != nil {
+		p.err("Remote connection failed: %s", err)
+		return
+	}
+	p.rconn = rconn
+	defer p.rconn.Close()
+	//display both ends
+	p.log("Opened %s >>> %s", p.Lconn.RemoteAddr().String(), p.rconn.RemoteAddr().String())
+	//bidirectional copy
+	go p.pipe(p.Lconn, p.rconn)
+	go p.pipe(p.rconn, p.Lconn)
+	//wait for close...
+	<-p.Errsig
+	p.log("Closed (%d bytes sent, %d bytes recieved)", p.sentBytes, p.receivedBytes)
+}
+
+func (p *Proxy) pipe(src, dst *net.TCPConn) {
+	//data direction
+	var f, h string
+	isFromDebugger := src == p.Lconn
+	if isFromDebugger {
+		f = "\nDebugger >>> IDE\n================"
+	} else {
+		f = "\nIDE >>> Debugger\n================"
+	}
+	h = "%s"
+	//directional copy (64k buffer)
+	buff := make([]byte, 0xffff)
+	for {
+		n, err := src.Read(buff)
+		if err != nil {
+			p.err("Read failed '%s'\n", err)
+			return
+		}
+		b := buff[:n]
+		p.log(h, f)
+		if veryverbose {
+			if isFromDebugger {
+				p.log("Raw protocol:\n%s\n", logger.Colorize(fmt.Sprintf(h, b), "blue"))
+			} else {
+				p.log("Raw protocol:\n%s\n", logger.Colorize(fmt.Sprintf(h, logger.FormatTextProtocol(b)), "blue"))
+			}
+		}
+		//extract command name
+		if isFromDebugger {
+			b = pathmapper.ApplyMappingToXML(b)
+		} else {
+			b = pathmapper.ApplyMappingToTextProtocol(b)
+		}
+		//show output
+		if veryverbose {
+			if isFromDebugger {
+				p.log("Processed protocol:\n%s\n", logger.Colorize(fmt.Sprintf(h, b), "blue"))
+			} else {
+				p.log("Processed protocol:\n%s\n", logger.Colorize(fmt.Sprintf(h, logger.FormatTextProtocol(b)), "blue"))
+			}
+		} else {
+			p.log(h, "")
+		}
+		//write out result
+		n, err = dst.Write(b)
+		if err != nil {
+			p.err("Write failed '%s'\n", err)
+			return
+		}
+		if isFromDebugger {
+			p.sentBytes += uint64(n)
+		} else {
+			p.receivedBytes += uint64(n)
+		}
+	}
+}
