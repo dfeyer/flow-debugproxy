e7b9c82cb0bc1b236b00b046e0b7ed686e869e1d
Dominique Feyer <dfeyer@ttree.ch>
2015-07-12 01:46:03 +0200

    [TASK] Add documentation comment in pathmapping module

--- pathmapping/pathmapping.go
+++ pathmapping/pathmapping.go
@@ -5,10 +5,12 @@ var mapping = map[string]string{}
 // PathMapping is a simple key store for class and proxy class mapping
 type PathMapping struct{}
 
+// Set a path mapping
 func (p *PathMapping) Set(path string, originalPath string) {
 	mapping[path] = originalPath
 }
 
+// Get a path mapping
 func (p *PathMapping) Get(path string) (string, bool) {
 	if p.Has(path) {
 		return mapping[path], true
@@ -16,6 +18,7 @@ func (p *PathMapping) Get(path string) (string, bool) {
 	return "", false
 }
 
+// Has check if the path mapping exist
 func (p *PathMapping) Has(path string) bool {
 	if _, exist := mapping[path]; exist {
 		return true
