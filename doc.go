14d0726aee970aaa00a811893ccb0f35cc6acef1
Dominique Feyer <dfeyer@ttree.ch>
2015-07-12 02:17:02 +0200

    [TASK] Add minimalistic doc.go and copyright notice

---
+++ doc.go
@@ -0,0 +1,11 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+  Package flow-debugproxy implements an xDebug proxy to
+  debug proxy class directly from user code. You can use
+  breakpoint directly in your code and the proxy did the
+  mapping between your code and the proxy class.
+*/
+package main
